212c9c3bd8d1815beefde00a30b0af20f7597823
gkozub <gkoz@diceus.com>
2018-01-05 15:13:38 +0200

    Add text length validation

--- src/ValidationComponent.js
+++ src/ValidationComponent.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
-const validationComponent = (props) => (
-  <p>{props.textLength}</p>
-);
+const validationComponent = (props) => {
+  let validationText = props.textLength > 5 ? 'Text is long enough' : 'Text is too short';
+
+  return (
+    <p>{validationText}</p>
+  )
+};
 
 export default validationComponent
